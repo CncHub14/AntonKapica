653a3e847fb07e63a5b9ce39d0450066a1db227d
AntonKapica <antoKap888@ya.ru>
2021-12-08 13:53:31 +0300

    Odin Itog

---
+++ app/src/main/java/com/example/odinitog/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.odinitog
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+        fun nextAc(view: View){
+//            val textview = findViewById<TextView>(R.id.textView3)
+            val nextActv = Intent(this, SecondMainActivity2::class.java)
+
+    startActivity(nextActv)
+    }
+
+    fun prevAct(view: android.view.View) {}
+}
